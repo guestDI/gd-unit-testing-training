92805abb2a28b06b1e1321f18a59767863643e2d
guestDI <ignatovich.dm@gmail.com>
2022-08-29 14:10:42 +0200

    initial commit

---
+++ __fixtures__/mocks.js
@@ -0,0 +1,82 @@
+export const tokenMocks = [
+    {
+        token: 'MM',
+        value: '08'
+    },
+    {
+        token: 'MMM',
+        value: 'Aug'
+    },
+    {
+        token: 'MMMM',
+        value: 'August'
+    },
+    {
+        token: 'M',
+        value: '8'
+    },
+    {
+        token: 'YYYY',
+        value: '2022'
+    },
+    {
+        token: 'YY',
+        value: '22'
+    },
+    {
+        token: 'DDD',
+        value: 'Monday'
+    },
+    {
+        token: 'DD',
+        value: 'Mon'
+    },
+    {
+        token: 'D',
+        value: 'Mo'
+    },
+    {
+        token: 'd',
+        value: '29'
+    },
+    {
+        token: 'ZZ',
+        value: '+0200'
+    },
+    {
+        token: 'Z',
+        value: '+02:00'
+    },
+    {
+        token: 'H',
+        value: '12'
+    },
+    {
+        token: 'HH',
+        value: '12'
+    },
+    {
+        token: 'hh',
+        value: '12'
+    },
+    {
+        token: 'h',
+        value: '12'
+    },
+    {
+        token: 'mm',
+        value: '12'
+    },
+    {
+        token: 'A',
+        value: 'PM'
+    },
+    {
+        token: 'a',
+        value: 'pm'
+    },
+    {
+        token: 'ff',
+        value: '000'
+    }
+]
---
+++ unitTestingTask.test.js
@@ -0,0 +1,81 @@
+import unitTestingTask from './unitTestingTask'
+import { tokenMocks } from './__fixtures__/mocks'
+
+const formatError = 'Argument `format` must be a string'
+const dateError = 'Argument `date` must be instance of Date or Unix Timestamp or ISODate String'
+
+describe('Date formats', () => {
+    beforeAll(() => {
+        jest
+            .useFakeTimers()
+            .setSystemTime(new Date(1661767962000))
+    })
+    
+    afterAll(() => {
+        jest.useRealTimers()
+    })
+
+    it('returns current date based on formatter', () => {
+        expect(unitTestingTask('ISODate')).toBe('2022-08-29')
+    })
+
+    it('returns custom date based on formatter', () => {
+        expect(unitTestingTask('ISODate', '2022-07-20T00:00:00')).toBe('2022-07-20')
+    })
+
+    it('throws an error if format is undefined', () => {
+        expect(() => { unitTestingTask(undefined, new Date()) }).toThrow(new TypeError(formatError));
+    })
+
+    it('throws an error if date param is passed as null', () => {
+        expect(() => { unitTestingTask('MM', null) }).toThrow(new TypeError(dateError));
+    })
+
+    it('returns default formatters', () => {
+        expect(unitTestingTask.formatters()).toStrictEqual(['ISODate', 'ISOTime', 'ISODateTime', 'ISODateTimeTZ'])
+    })
+
+    it('creates custom formatter', () => {
+        unitTestingTask.register('CustomFormatter', 'YYYY-MM')
+        expect(unitTestingTask.formatters()).toStrictEqual(['ISODate', 'ISOTime', 'ISODateTime', 'ISODateTimeTZ', 'CustomFormatter'])
+    })
+
+    describe('tokens functions', () => {
+        tokenMocks.forEach(({token, value}) => {
+            it(`returns date in required format: ${token}`, () => {
+                expect(unitTestingTask(token, new Date())).toBe(value)
+            })
+        })
+    })
+
+    describe('languages functions', () => {
+        it('should return default language if no language provided', () => {
+            expect(unitTestingTask.lang()).toBe('en')
+        })
+
+        it('should return default language if no options', () => {
+            expect(unitTestingTask.lang('de')).toBe('en')
+        })
+
+        it('should return correct meridiem in lowerCase for time after 11', () => {
+            expect(unitTestingTask._languages['en'].meridiem(12, true)).toBe('pm')
+        })
+
+        it('should return correct meridiem in upperCase for time after 11', () => {
+            expect(unitTestingTask._languages['en'].meridiem(12, false)).toBe('PM')
+        })
+
+        it('should return correct meridiem in upperCase for time before 11', () => {
+            expect(unitTestingTask._languages['en'].meridiem(10, false)).toBe('AM')
+        })
+
+        it('should return correct meridiem in the lowerCase for time before 11', () => {
+            expect(unitTestingTask._languages['en'].meridiem(10, true)).toBe('am')
+        })
+
+        it('should return correct language different from default one', () => {
+            unitTestingTask.lang('pl', {})
+            expect(unitTestingTask.lang('en')).toBe('en')
+        })
+    })
+})
